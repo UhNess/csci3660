c6090be06822b69fb05d16b6046e186a90d9e37c
UhNess <jokes0607@yahoo.com>
2025-02-06 14:16:16 -0500

    added our names + info into ABOUT

--- stickersmash/app/(tabs)/about.tsx
+++ stickersmash/app/(tabs)/about.tsx
@@ -3,10 +3,27 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>About Us</Text>
+      <Text style={styles.text}>CSCI 3660, Mobile Application Development</Text>
+      <Text style={styles.text}>Group 3 Members:</Text>
+      <Text style={styles.text}>Sam Charles, 
+        Destani Fountain, 
+        Walden Hackle, 
+        Josh Ochs, 
+        Clark Serecky, 
+        Damien Smith
+      </Text>
+      
+      <Text style={styles.text}>To log in:</Text>
+      <Text style={styles.text}>Username: admin</Text>
+      <Text style={styles.text}>Password: password123</Text>
     </View>
   );
 }
+
+/*
+this is just something to add
+*/
 
 const styles = StyleSheet.create({
   container: {
