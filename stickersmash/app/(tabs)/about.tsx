00095666c4c3d0cca8e54569ac92b2b48c8517ea
UhNess <jokes0607@yahoo.com>
2025-02-06 13:10:56 -0500

    Added names and some info in ABOUT

--- stickersmash/app/(tabs)/about.tsx
+++ stickersmash/app/(tabs)/about.tsx
@@ -3,7 +3,16 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>About Us</Text>
+      <Text style={styles.text}>CSCI 3660, Mobile Application Development</Text>
+      <Text style={styles.text}>Group 3 Members:</Text>
+      <Text style={styles.text}>Sam Charles, 
+        Destani Fountain, 
+        Walden Hackle, 
+        Josh Ochs, 
+        Clark Serecky, 
+        Damien Smith
+      </Text>
     </View>
   );
 }
